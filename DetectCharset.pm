8bcf21778c1bb1bd51f45f4eddfb598fbbf47d06
Meettya <meettya@gmail.com>
2010-05-25 19:35:49 +0400

    fix property

--- DetectCharset.pm
+++ DetectCharset.pm
@@ -67,7 +67,8 @@ sub detect_text {
 	return -1 if( !defined $res[0] || $all_res->{$res[0]} < 1 );
 
 	if( $all_res->{$res[0]} > $all_res->{$res[1]}*
-			( $self->can('min_diff') ? $self->min_diff() : $min_diff ) ){
+			( $self->can('min_diff') and defined $self->min_diff ?
+								$self->min_diff : $min_diff ) ){
 		return wantarray ? ( $res[0], $all_res->{$res[0]} ) : $res[0] }
 
 	return 0;
@@ -87,7 +88,8 @@ sub detect_file {
 		$kode =~ /^[a-z]/i ? ( $rezalt{$kode} += $ball ) : next ;
 
 	  if ( keys ( %rezalt ) == 1 && $rezalt{$kode} > 
-	  		( $self->can( 'min_file_size' ) ? $self->min_file_size : $min_file_size ) )
+	  		( $self->can( 'min_file_size' ) and defined $self->min_file_size ?
+	  						$self->min_file_size : $min_file_size ) )
 		{
 			close $fh;
 			return $kode;
@@ -97,7 +99,8 @@ sub detect_file {
 			my ( $leader, $second ) = sort { $rezalt{$b} <=> $rezalt{$a} }
                 keys %rezalt;
 			if ( $rezalt{$leader} >
-			$rezalt{$second}*( $self->can( 'min_diff' ) ? $self->min_diff : $min_diff ) ){
+			$rezalt{$second}*( $self->can( 'min_diff' ) and defined $self->min_diff ?
+							$self->min_diff : $min_diff ) ){
 				$ret = $leader;
 			}
 			$ret = 0;
--- test_more.t
+++ test_more.t
@@ -8,7 +8,7 @@ use Encode qw(encode decode);
 use Test::More tests => 8; # 4 codetest
 
 use_ok( 'DetectCharset', qw(detect_text detect_file) );
-can_ok('DetectCharset', qw(detect_text detect_file) );
+can_ok( 'DetectCharset', qw(detect_text detect_file) );
 
 
 my $test_text = 'Съешь еще этих мягких французских булок, да выпей чаю.'; 
@@ -16,7 +16,10 @@ my $test_text = 'Съешь еще этих мягких французских 
 my %rec_ok = map { $_, encode ( $_, decode("UTF-8", $test_text) ) } 
 						qw(UTF-8 CP1251 KOI8-R ISO-8859-5 CP866);
 
-my $ch_d = new DetectCharset;
+my $ch_d = new DetectCharset (qw(min_diff min_file_size));
+
+$ch_d->set_multi( min_diff => 2.5, min_file_size => 4_000_000 );
+
 print "\nTest encoding recognition:\n";
 ok ( $ch_d->detect_text($rec_ok{$_}) eq $_ , "$_ recognized" ) for keys %rec_ok;
 print "\nTest file encoding recognition:\n";
